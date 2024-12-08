a45300558ea5e0d777ab0731477b393c47c8547e
barto159 <bartosiak789@gmail.com>
2024-12-08 16:46:48 +0100

    zejsciowka 5

--- foo.hpp
+++ foo.hpp
@@ -10,7 +10,10 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
 
-    // Twoja implementacja tutaj
+    std::transform(people.rbegin(), people.rend(), retval.begin(), [](Human& person) {
+        person.birthday();                     
+        return person.isMonster() ? 'n' : 'y'; 
+    });
 
     return retval;
 }
